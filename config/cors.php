e60539df18ab3b37e8c3067ff08f2ef94ee05c66
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-01-08 18:26:28 +0700

    add cors for docs.trustlab.dyzulk.com

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'https://trustlab.dyzulk.com,https://api.trustlab.dyzulk.com,docs.trustlab.dyzulk.com,https://trustlab.pages.dev')),
+    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'https://trustlab.dyzulk.com,https://api.trustlab.dyzulk.com,https://docs.trustlab.dyzulk.com,https://trustlab.pages.dev')),
 
     'allowed_origins_patterns' => [
         '#^https?://.*\.trustlab\.pages\.dev$#',
